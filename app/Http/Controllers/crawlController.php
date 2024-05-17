0beb3c57e48b3249bd48449d349e97a17bdc95f5
cuong12121 <cuongdao12121@gmail.com>
2024-05-17 13:51:47 +0700

    Update crawlController.php

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -397,48 +397,51 @@ class crawlController extends Controller
     
         dd($check);
 
-        die;
-        $url = 'https://dienmaycholon.vn/may-lanh/may-lanh-comfee-inverter-15-hp-cfs13vaffv';
+        foreach ($check as $key => $value) {
+            // code...
+       
+            $url = 'https://dienmaycholon.vn'.trim($value);
 
-        $now = Carbon::now();
+            $now = Carbon::now();
 
-        $html = file_get_html(trim($url));
+            $html = file_get_html(trim($url));
 
-        $details = $html->find('.des_pro', 0);
+            $details = $html->find('.des_pro', 0);
 
-        $specifications = $html->find('.list_specifications', 0);
+            $specifications = $html->find('.list_specifications', 0);
 
-        $title =  strip_tags($html->find('.name_pro_detail h1', 0));  
+            $title =  strip_tags($html->find('.name_pro_detail h1', 0));  
 
-        // tính năng nổi bật
+            // tính năng nổi bật
 
-        $feature_item = $html->find('.feature_item',0);
+            $feature_item = $html->find('.feature_item',0);
 
-        $pattern = '/<a\s+[^>]*>(.*?)<\/a>/i';
+            $pattern = '/<a\s+[^>]*>(.*?)<\/a>/i';
 
-        // Replacement string (empty)
-        $replacement = '';
+            // Replacement string (empty)
+            $replacement = '';
 
-        // Perform the replacement
-        $details = preg_replace($pattern, $replacement, html_entity_decode($details));
+            // Perform the replacement
+            $details = preg_replace($pattern, $replacement, html_entity_decode($details));
 
-        $price = 0;
+            $price = 0;
 
-        // echo(html_entity_decode($feature_item));
-        
-        $data['Name']   = $title;
-        $data['Price']  = $price;
-        $data['Detail'] = $details;
-        $data['Link'] = convertSlug($title);
-        $data['Group_id']= 4;
-        $data['Specifications'] = $specifications;
-        $data['user_id'] = 4;
-        $data['created_at'] = $now;
-        $data['updated_at'] = $now;
-        $data['Salient_Features'] = $feature_item;
-        $data['crawl_link'] = $url;
-        
-        DB::table('products')->insert($data);
+            // echo(html_entity_decode($feature_item));
+            
+            $data['Name']   = $title;
+            $data['Price']  = $price;
+            $data['Detail'] = $details;
+            $data['Link'] = convertSlug($title);
+            $data['Group_id']= 4;
+            $data['Specifications'] = $specifications;
+            $data['user_id'] = 4;
+            $data['created_at'] = $now;
+            $data['updated_at'] = $now;
+            $data['Salient_Features'] = $feature_item;
+            $data['crawl_link'] = $url;
+            
+            DB::table('products')->insert($data);
+        }     
         echo "thành công";
 
         // dd($details);
