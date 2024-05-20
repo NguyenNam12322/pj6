41067eb233445d79fca899450db36913abd55d87
cuong12121 <cuongdao12121@gmail.com>
2024-05-20 16:40:19 +0700

    Update crawlController.php

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -528,7 +528,7 @@ class crawlController extends Controller
                     mkdir($directory, 0777, true);
                 }
 
-                $link_crawl = 'http:'.trim($value);
+                $link_crawl = 'http:'.str_replace('https:', '', trim($value));
 
 
                 $img = $directory.'/'.basename($value);
