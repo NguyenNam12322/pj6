92b59f1dc88d14849fc5846595d329c09288f2d9
cuong12121 <cuongdao12121@gmail.com>
2024-05-22 15:45:53 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -472,7 +472,43 @@
 
                                                 }
                                             ?>
-                                            <div class="product col-md-3 col-xs-6">
+                                            <div class="product col-md-3 col-xs-6 view-show-hide">
+                                                <div class="product-header" href="{{ route('details', $datas->Link) }}">
+                                                    <div class="top-right">
+                                                        <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
+                                                    </div>
+                                                    <div class="product-image">
+                                                        <a href="{{ route('details', $datas->Link) }}">
+                                                            <img
+                                                                class="ls-is-cached lazyloaded"
+                                                                
+                                                                src="{{ asset($datas->Image) }}"
+                                                                
+                                                            />
+                                                        </a>
+                                                    </div>
+                                                    
+                                                </div>
+                                                <div class="product-body">
+                                                    <div class="product-feature-badge"></div>
+                                                    <div class="product-title"><a href="{{ route('details', $datas->Link) }}">{{ $datas->Name }}</a></div>
+                                                    <div class="product-price">
+                                                        <p class="final-price">{{ @str_replace(',' ,'.', number_format($datas->Price)) }}đ   </p>
+                                                        
+                                                    </div>
+                                                </div>
+                                                <div class="product-footer"></div>
+                                            </div>
+                                          
+                                      
+
+                                        @endforeach
+
+
+                                        @foreach($data as $key =>$datas)
+
+                                           
+                                            <div class="product col-md-3 col-xs-6 view-show-all" >
                                                 <div class="product-header" href="{{ route('details', $datas->Link) }}">
                                                     <div class="top-right">
                                                         <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
@@ -731,8 +767,17 @@
         movingText.hide();
       });
 
+    $('.view-show-all').hide();  
 
 
+    $('.view-show-hide').click(function () {
+        $(this).remove();
+        $('.view-show-hide').remove();
+        $('.view-show-all').show();  
+
+    })  
+
+    
 
 
     
