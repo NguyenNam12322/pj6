f2908455e832eb71eb48ae5db6c438d5c7a2a0a9
cuong12121 <cuongdao12121@gmail.com>
2024-05-22 09:43:37 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -56,9 +56,9 @@
              ?>
 
             <title>{{  !empty($name_cates_cate)?$name_cates_cate:$meta->meta_title }}</title>
-            <meta name="description" content="{{ $meta->meta_content }}"/>
-            <meta property="og:title" content="{{ $meta->meta_title }}" />
-            <meta property="og:description" content="{{ $meta->meta_content }}" /> 
+            <meta name="description" content="{{ $meta->meta_content??''}}"/>
+            <meta property="og:title" content="{{ $meta->meta_title??'' }}" />
+            <meta property="og:description" content="{{ $meta->meta_content??'' }}" /> 
             <meta name="keywords" content="{{ $meta->meta_key_words??'sieu thi dien may, siêu thị điện máy, mua điện máy giá rẻ, siêu thị điện máy uy tín, siêu thị điện máy trực tuyến' }}"/>
             @endif
         @endif
