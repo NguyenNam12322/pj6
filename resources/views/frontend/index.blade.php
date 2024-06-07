e4fe3f814efa826ec916ef628268dad9f30aadb0
cuong12121 <cuongdao12121@gmail.com>
2024-06-07 14:16:10 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -169,6 +169,8 @@
 </style>
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/details.css') }}?ver=25">
+
+<div class="gift-info"></div>
 <div class="row-fluid ">
     <div class="span16 container outstanding">
         <div class="row-fluid ">
