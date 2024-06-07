f1363b793c7bdccd4a2b384bff523a8721bec7f6
cuong12121 <cuongdao12121@gmail.com>
2024-06-07 11:46:56 +0700

    Update index.blade.php

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -337,7 +337,7 @@
                                         @endif
                                     </a>
                                 </div>
-                                 <h2>test</h2>
+                                 
                             @include('frontend/layouts/more-info', ['value'=>$count_pd, 'deal_price'=>$value->deal_price])
                             </div>
 
