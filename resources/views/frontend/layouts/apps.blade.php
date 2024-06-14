1f6e9b9c3c1d4637ae286207939f474790c0104c
cuong12121 <cuongdao12121@gmail.com>
2024-06-14 13:47:23 +0700

    update

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -57,7 +57,9 @@
 
             <title>{{  !empty($name_cates_cate)?$name_cates_cate:$meta->meta_title }}</title>
             <meta name="description" content="{{ $meta->meta_content??''}}"/>
-            <meta property="og:title" content="{{ $meta->meta_title??'' }}" />
+
+
+            <meta property="og:title" content="{{  !empty($meta->meta_title)?$meta->meta_title:'' }}" />
             <meta property="og:description" content="{{ $meta->meta_content??'' }}" /> 
             <meta name="keywords" content="{{ $meta->meta_key_words??'sieu thi dien may, siêu thị điện máy, mua điện máy giá rẻ, siêu thị điện máy uy tín, siêu thị điện máy trực tuyến' }}"/>
             @endif
