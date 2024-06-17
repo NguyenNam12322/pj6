69fe6cad3d659c88a14e75e4797760d98b97cef1
cuong12121 <cuongdao12121@gmail.com>
2024-06-17 15:32:15 +0700

    update

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -1871,100 +1871,11 @@
     <div class="max-width">
         <div class="phone_hotline"> <a href="tel:0243.687.9145" title="Bảo Hành: 0243.687.9145" class="p_hotline_item"> <i class="icon_security"></i> <span><strong>Bảo Hành: 0243.687.9145</strong> (8h00 - 17h00)</span> </a> <a href="tel:0247.303.6336" title="Mua hàng: 0247.303.6336" class="p_hotline_item"> <i class="icon_purchase"></i> <span><strong>Mua hàng: 0247.303.6336</strong> (8h00 - 17h00)</span> </a> <a href="tel:0916917949" title="Khiếu nại:0916917949" class="p_hotline_item"> <i class="icon_complain"></i> <span><strong>Khiếu nại: 091.691.7949</strong> (8h00 - 17h00)</span> </a> </div>
     </div>
+
+    @include('frontend.footer')
        
 
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-2 col-footer">
-                        <h3 class="title">Thông tin</h3>
-                        <ul class="footer_list-link">
-                            <li>
-                                <a href="{{ route('details', 'gioi-thieu') }}">Giới thiệu</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'lien-he') }}">Liên hệ</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'nha-phan-phoi-dien-may-uy-tin-chuyen-nghiep') }}">Dự án bán buôn</a>
-                            </li>
-
-                          
-                            <li class="mobile">
-                                <?php 
-
-                                    $routeUri = Route::getFacadeRoot()->current();
-                                ?>
-                                @if(!empty($routeUri))
-                                <a href="{{ Route::getFacadeRoot()->current()->uri() }}?show=pc_view">Xem bản desktop</a>
-                                @endif
-                            </li>
-                        </ul>
-                    </div>
-
-                   
-                    <div class="col-md-3 col-footer">
-                        <h3 class="title">Hỗ trợ mua hàng</h3>
-                        <ul class="footer_list-link">
-                            <li>
-                                <a href="{{ route('details','huong-dan-mua-dien-may-tra-gop-online-qua-the-tin-dung-tren-trang-web-dien-may-nguoi-viet') }}">Hướng dẫn trả góp</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'page/cach-thuc-thanh-toan') }}">Cách thức thanh toán</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'bang-gia-vat-tu-lap-dat') }}">Bảng giá vật tư lắp đặt</a>
-                            </li>
-                        </ul>
-                    </div>
-                    
-                    <div class="col-md-4 col-footer">
-                        <h3 class="title">Chính sách</h3>
-                        <ul class="footer_list-link">
-                            <li>
-                                <a href="/page/chinh-sach-quy-dinh-chung">Chính sách &amp; quy định chung</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'page/chinh-sach-doi-tra-hang') }}">Chính sách đổi trả sản phẩm</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'page/chinh-sach-bao-hanh') }}">Chính sách bảo hành</a>
-                            </li>
-                            <li>
-                                <a href="{{ route('details', 'page/chinh-sach-van-chuyen') }}">Chính sách giao hàng</a>
-                            </li>
-                        </ul>
-                    </div>
-                    <!-- <div class="col-md-3 col-footer">
-                        <form>
-                            <input id="email_newsletter" type="text" placeholder="Đăng ký email nhận thông tin khuyến mại">
-                            <span id="now_submit">Gửi</span>
-                        </form>
-
-                        <h4 style="margin-top: 20px;margin-bottom: 10px;">Kết nối với chúng tôi</h4>
-                        <a class="ft-fb" rel="nofollow" href="https://www.facebook.com/dienmaynguoiviet/"><i class="fab fa-facebook-f"></i></a>
-                        <a class="ft-yt" rel="nofollow" href="https://www.youtube.com/channel/UCRVWFSZs8k81B61_hwmkMIA"><i class="fab fa-youtube"></i></a>
-                        <a rel="nofollow" href="http://online.gov.vn/HomePage/CustomWebsiteDisplay.aspx?DocId=1180"><img style="max-height: 40px" src="{{ asset('images/template/dathongbao.png') }}"></a>
-                    </div> -->
-                </div>
-                <!-- <div class="footer-bottom">
-                    <div class="container">
-                        <p> <b>© 2018. Công ty TNHH Thương Mại Phú Tiến. Địa chỉ: : Kho Đóng Tàu, Ngõ 683 Đường Nguyễn Khoái, Quận Hoàng
-                            Mai, TP HN. GPKD số : 0102011440 do Sở Kế Hoạch và Đầu Tư TP. Hà Nội, cấp ngày 25/02/2004 </b>
-                        </p>
-                    </div>
-                </div> -->
-            </div>
-        </footer>
-
         
-
-        <div class="hotlines">   
-            <a href="tel:02473036336"><img src="{{ asset('images/template/tongdai02473036336.png') }}" alt="hotline" style="height:55px; z-index: 999;"></a>
-              
-            <div class="clear"></div>
-              
-        </div>
 
        <!--  <div class="re-call">   
             <a href="tel:02473036336"><img src="{{ asset('images/template/tongdai02473036336.png') }}" alt="hotline" style="height:55px; z-index: 999;"></a>
