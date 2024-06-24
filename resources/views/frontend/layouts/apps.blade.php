359f2928c58dd0fb3bd9c9e27f5ce18c00d7f36c
cuong12121 <cuongdao12121@gmail.com>
2024-06-24 09:01:57 +0700

    update

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -73,7 +73,7 @@
         <meta http-equiv="Cache-control" content="public">
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}?ver=41 ">
+         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}?ver=46 ">
 
          <style type="text/css">
             .nk-nav-right ul{
