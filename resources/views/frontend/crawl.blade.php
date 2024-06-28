d3a6d608886a3a967f47e5e7665cceda01494542
cuong12121 <cuongdao12121@gmail.com>
2024-06-28 15:19:37 +0700

    update

--- resources/views/frontend/crawl.blade.php
+++ resources/views/frontend/crawl.blade.php
@@ -9439,4 +9439,18 @@
             </div>
         </div>
     </div>
+
+    <script type="text/javascript">
+        if ('serviceWorker' in navigator) {
+            window.addEventListener('load', function() {
+                navigator.serviceWorker.register('/sw.js')
+                    .then(function(registration) {
+                        console.log('Service worker registered:', registration);
+                    })
+                    .catch(function(error) {
+                        console.log('Service worker registration failed:', error);
+                    });
+            });
+        }
+    </script>
 </div>
