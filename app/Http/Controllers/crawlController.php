752ac9e32fee308712076c0de221740451bc7693
cuong12121 <cuongdao12121@gmail.com>
2024-07-03 16:25:00 +0700

    update

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -797,7 +797,7 @@ class crawlController extends Controller
 
             $html = file_get_html(trim($link));
 
-            $src = $html->find('.product-images .wp-post-image img', 0);
+            $src = $html->find('.product-images .wp-post-image', 0);
 
             dd($src);
 
