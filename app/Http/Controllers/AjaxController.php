eb99a38ccf332375295b5f8d375b119b18ac0545
cuong12121 <cuongdao12121@gmail.com>
2024-08-07 16:43:00 +0700

    update

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -800,7 +800,7 @@ class AjaxController extends Controller
             $price = trim($data_Product->Price);
         }
              
-        Cart::add(['id' => $id, 'name' => $data_Product->Name,  'qty' => 1, 'price' => $price, 'weight' => '500',  'options' => ['link' => $data_Product->Link, 'gift'=>$gift??'', 'transportation_cost'=> intval($price)-intval($newPrice)]]);
+        Cart::add(['id' => $id, 'name' => $data_Product->Name,  'qty' => 1, 'price' => $price, 'weight' => '500',  'options' => ['link' => $data_Product->Link, 'gift'=>$gift??'', 'transportation_cost'=> intval($newPrice)]]);
 
         $data_cart = Cart::content();
 
