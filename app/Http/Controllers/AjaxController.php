26ddc91a9ca89d42aebcb43517695ffe911f7140
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-08-07 20:34:32 +0700

    up

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -800,7 +800,7 @@ class AjaxController extends Controller
             $price = trim($data_Product->Price);
         }
              
-        Cart::add(['id' => $id, 'name' => $data_Product->Name,  'qty' => 1, 'price' => $price, 'weight' => '500',  'options' => ['link' => $data_Product->Link, 'gift'=>$gift??'', 'transportation_cost'=> intval($newPrice[0])- intval($price)]]);
+        Cart::add(['id' => $id, 'name' => $data_Product->Name,  'qty' => 1, 'price' => $price, 'weight' => '500',  'options' => ['link' => $data_Product->Link, 'gift'=>$gift??'', 'transportation_cost'=> !empty($newPrice[0])?intval($newPrice[0])- intval($price):0]]);
 
         $data_cart = Cart::content();
 
