424ed3e4d433303a7dc67d12a9412f85ff824b6a
cuong12121 <cuongdao12121@gmail.com>
2024-08-28 16:38:40 +0700

    update

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -1540,7 +1540,7 @@
             <div class="box-compare">
 
                 @if(!empty($thuonghieu[$data_cate])&& !empty($ar_groups_info))
-                <span style="font-weight: bold; font-size:17px ">Hãng:  <a href="{{ route('details', $ar_groups_info[0]['link']) }}">{{ str_replace($namecate->name, '',  $ar_groups_info[0]['name']) }}</a></span> 
+                <span style="font-weight: bold; font-size:17px ">Hãng:  <a href="#">{{ str_replace($namecate->name, '',  $ar_groups_info[0]['name']) }}</a></span> 
                 @endif
                 &nbsp
 
