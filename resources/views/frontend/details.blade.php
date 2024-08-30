a60f2f5fb102bf787d271fd14bffec63f9bbe07a
cuong12121 <cuongdao12121@gmail.com>
2024-08-30 11:50:26 +0700

    update

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -2346,7 +2346,7 @@
 
 </div>
 @push('style')
-<link rel="stylesheet" type="text/css" href="{{ asset('css/details.css') }}?ver=25">
+<link rel="stylesheet" type="text/css" href="{{ asset('css/details.css') }}?ver=26">
 @endpush
 @push('script')
 
