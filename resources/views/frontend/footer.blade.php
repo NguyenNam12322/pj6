f82c78c4b77b80e3d4bdab6f8a6e0e1a06ccab81
cuong12121 <cuongdao12121@gmail.com>
2024-08-30 15:25:00 +0700

    update

--- resources/views/frontend/footer.blade.php
+++ resources/views/frontend/footer.blade.php
@@ -26,6 +26,10 @@
             font-weight: bold;
         }
 
+        .to-top{
+            background: #ddd;
+        }
+
 
 
         .coccoc-alo-phone {
@@ -728,8 +732,33 @@
         </div>
     </div>
 
-     <div class="ring-phone to-top">
+     <div id="scrollToTopBtn" class="ring-phone to-top" style="display:none"  >
         <img src="{{ asset('images/template/back-top.png') }}">
     </div>    
 
 </footer>
+
+<script type="text/javascript">
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+scrollToTopBtn.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+</script>
