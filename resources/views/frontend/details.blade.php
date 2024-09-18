ab333bc9e001602949a04cfb44e638d445f65343
cuong12121 <cuongdao12121@gmail.com>
2024-09-18 14:11:34 +0700

    update

--- resources/views/frontend/details.blade.php
+++ resources/views/frontend/details.blade.php
@@ -885,7 +885,7 @@
                         </div>
 
                         <div class="pdetail-stockavailable">
-                            <span>{{ $status }}</span>
+                            <span>  @if($status==="CÒN HÀNG") <img src="{{ asset('images/template/icon-tick.png') }}" width="18px" height="18px" >  {{ $status }}</span>
                         </div>
                         <div class="scroll-box">
                             <div class="pdetail-price">
