ae4e83e65db78af127c9f3bf25d7a4ed941f4faa
cuong12121 <cuongdao12121@gmail.com>
2024-09-20 09:57:00 +0700

    update

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -404,6 +404,16 @@
                             </div>
                         </div>
 
+                        <nav aria-label="Page navigation example">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="?page=1">1</a></li>
+                                <li class="page-item"><a class="page-link" href="?page=2">2</a></li>
+                                <li class="page-item"><a class="page-link" href="?page=3">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>
+
                          @else
 
                         <h3>Không tìm thấy sản phẩm ở mục đã chọn</h3>
