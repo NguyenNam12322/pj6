157373fc7be6f12f2319e5ea915c8388c8fcae3f
cuong12121 <cuongdao12121@gmail.com>
2024-09-20 16:45:45 +0700

    update

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -1433,7 +1433,7 @@ class crawlController extends Controller
     {
         $now = Carbon::now();
 
-        $data = DB::table('products')->select('crawl_link','id')->orderBy('id','asc')->where('id','>',1491)->get();
+        $data = DB::table('products')->select('crawl_link','id')->orderBy('id','asc')->where('id','>',1520)->get();
 
 
         foreach ($data as $key => $values) {
