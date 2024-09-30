9a99d25c9d14759a3561bb3809ebca910e58a792
cuong12121 <cuongdao12121@gmail.com>
2024-09-30 09:48:34 +0700

    update

--- resources/views/frontend/category.blade.php
+++ resources/views/frontend/category.blade.php
@@ -135,7 +135,7 @@
                                         @endforeach
                                         @endif
 
-                                        <li>{{ $meta->meta_title }}</li>
+                                        <li>{{ $meta->id }}</li>
                                     </ul>
                                 </section>
                             </div>
