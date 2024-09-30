d7a1700903f1ae14ba2a75071129976442efff86
cuong12121 <cuongdao12121@gmail.com>
2024-09-30 09:59:39 +0700

    update

--- app/Http/Controllers/sitemapController.php
+++ app/Http/Controllers/sitemapController.php
@@ -14,6 +14,7 @@ class sitemapController extends Controller
 {
    public function index()
    {
+        return response()->view('sitemap.index')->header('Content-Type', 'text/xml');
    }    
    public function sitemapChildProduct()
    {
