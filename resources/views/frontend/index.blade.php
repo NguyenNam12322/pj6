db2cb414160f9caab184f01e7bf1f973e7c06974
cuong12121 <cuongdao12121@gmail.com>
2024-10-09 14:56:45 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -701,7 +701,7 @@
 
                                                 }
                                             ?>
-                                            <div class="product col-md-3 col-xs-6 view-show-hide item" id="item_feature-{{ $datas->id }}">
+                                            <div class="product col-md-2 col-xs-6 view-show-hide item" id="item_feature-{{ $datas->id }}">
                                                 <div class="product-header" href="{{ route('details', $datas->Link) }}">
                                                     <div class="top-right">
                                                         <div class="product-feature-badge-item installment"><span>Trả góp 0%</span></div>
