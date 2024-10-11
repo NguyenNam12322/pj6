59ca9a12f6dad2c59cb330aaa22585bed459dbd9
cuong12121 <cuongdao12121@gmail.com>
2024-10-11 11:21:20 +0700

    update

--- resources/views/frontend/page_mobile/details.blade.php
+++ resources/views/frontend/page_mobile/details.blade.php
@@ -4,17 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="{{ asset('mobile/assets/75a5fa0c/css/progressive-media.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('mobile/assets/35deb2b4/css/bootstrap.css') }}" rel="stylesheet">
-    <link href="{{ asset('mobile/bulma/css/bulma.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/75a5fa0c/css/progressive-media.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('assets/35deb2b4/css/bootstrap.css') }}" rel="stylesheet">
+   <!--  <link href="{{ asset('mobile/bulma/css/bulma.min.css') }}" rel="stylesheet">
     <link href="{{ asset('mobile/bulma/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('swiper@8.4.2/swiper-bundle.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('mobile/css/owl.carousel.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('mobile/css/owl.theme.default.min.css')}}  " rel="stylesheet">
-    <link href="{{ asset('mobile/css/icofont.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('mobile/css/slick.css')}}" rel="stylesheet">
-    <link href="{{ asset('mobile/css/slick-theme.css')}}" rel="stylesheet">
-    <link href="{{ asset('mobile/css/site.css')}}" rel="stylesheet">
+    <link href="{{ asset('swiper@8.4.2/swiper-bundle.min.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/owl.theme.default.min.css')}}  " rel="stylesheet">
+    <link href="{{ asset('css/icofont.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/slick.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/slick-theme.css')}}" rel="stylesheet">
+    <link href="{{ asset('css/site.css')}}" rel="stylesheet">
      <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}?ver=56 ">
 </head>
 <body>
