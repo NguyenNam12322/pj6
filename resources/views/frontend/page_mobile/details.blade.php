ea4795d93fb9f2d2c2cb0e11c38465ec7a4ad527
cuong12121 <cuongdao12121@gmail.com>
2024-10-11 11:19:56 +0700

    update

--- resources/views/frontend/page_mobile/details.blade.php
+++ resources/views/frontend/page_mobile/details.blade.php
@@ -6,9 +6,9 @@
     <title>Document</title>
     <link href="{{ asset('mobile/assets/75a5fa0c/css/progressive-media.min.css') }}" rel="stylesheet">
     <link href="{{ asset('mobile/assets/35deb2b4/css/bootstrap.css') }}" rel="stylesheet">
-   <!--  <link href="{{ asset('mobile/bulma/css/bulma.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('mobile/bulma/css/bulma.min.css') }}" rel="stylesheet">
     <link href="{{ asset('mobile/bulma/css/all.min.css') }}" rel="stylesheet">
-    <link href="{{ asset('swiper@8.4.2/swiper-bundle.min.css') }}" rel="stylesheet"> -->
+    <link href="{{ asset('swiper@8.4.2/swiper-bundle.min.css') }}" rel="stylesheet">
     <link href="{{ asset('mobile/css/owl.carousel.min.css') }}" rel="stylesheet">
     <link href="{{ asset('mobile/css/owl.theme.default.min.css')}}  " rel="stylesheet">
     <link href="{{ asset('mobile/css/icofont.min.css') }}" rel="stylesheet">
