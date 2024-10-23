217361317b74f6728b573f0b40f748a06ce01a0f
cuong12121 <cuongdao12121@gmail.com>
2024-10-23 11:04:22 +0700

    update

--- resources/views/frontend/blogdetail.blade.php
+++ resources/views/frontend/blogdetail.blade.php
@@ -42,7 +42,7 @@
             <ul class="dm_container">
                 <li>
                     <a href="/tu-van-ti-vi/">
-                    <img src="{{ asset('images/template/logo/ti-vi.png') }}" alt="">
+                    <img src="{{ asset('images/template/logo/tivi.png') }}" alt="">
                     <span>Tư vấn 
                     <br> tivi</span>
                     </a>
