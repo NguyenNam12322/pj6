f12dd23c6f4a00943b383d1e7c2881c454abd6bd
cuong12121 <cuongdao12121@gmail.com>
2024-11-01 11:17:04 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -56,19 +56,7 @@
         <link rel="preload" href="{{asset('css/lib/owl.theme.default.min.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">
         <link rel="preload" href="{{ asset('css/lib/bootstrap.min.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 
-        <script rel="preload" href="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
-
-        <!-- <link rel="preload" href="https://code.jquery.com/ui/1.13.1/jquery-ui.js" as="script" onload="this.onload=null;document.body.appendChild(Object.assign(document.createElement('script'), {src: this.href}));"> -->
-
-        <link rel="preload" href="{{ asset('js/layout1.js') }}" as="script">
-   
-        <link rel="preload" href="{{asset('js/lib/owl.carousel.min.js')}}" as="script">
-
-      
-        <link rel="preload" href="{{ asset('js/lib/bootstrap.min.js') }}" as="script">
-
-        <link rel="preload" href="{{asset('js/lib/jquery.validate.min.js')}}" as="script">
-        <link rel="preload" href="{{ asset('js/lib/lazyload.js') }}" as="script">
+        <link rel="preload" href="https://code.jquery.com/ui/1.13.1/jquery-ui.js" as="script">
 
 
          <style type="text/css">
