8b2b4db7189517b25587e1e5f7433506e3318b83
cuong12121 <cuongdao12121@gmail.com>
2024-11-01 14:10:01 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -1897,7 +1897,7 @@
 
 
 
-    <script   src="{{ asset('js/layout1.js') }}" ></script>
+    <script   src="{{ asset('js/layout1.js') }}" defer></script>
 
     <link rel="preload" href="{{asset('js/lib/owl.carousel.min.js')}}" as="script">
   
