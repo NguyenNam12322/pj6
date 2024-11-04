a25e35779d88133ceada9caf020870ff94c370dd
cuong12121 <cuongdao12121@gmail.com>
2024-11-04 16:29:39 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -1543,8 +1543,8 @@
         <div class="zalo-mobile">
 
             <a href="https://zalo.me/0987874334" target="_blank">
-                <div style="position: fixed; bottom: 52px; right: 52px; transform: translate(0px, 0px) !important; z-index: 2147483644; border: none; visibility: visible; right: 0px; width: 30px; height: 30px;" class="zalo-chat-widget lazyload" data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height="">
-                    <img src="{{ asset('public/images/template/image.png') }}" width="30" height="30"  data-src="{{ asset('public/images/template/Logo.svg') }}" loading="lazy">
+                <div style="position: fixed; bottom: 52px; right: 52px; transform: translate(0px, 0px) !important; z-index: 2147483644; border: none; visibility: visible; right: 0px; width: 30px; height: 30px;" class="zalo-chat-widget" data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height="">
+                    <img class="lazyload" src="{{ asset('public/images/template/image.png') }}" width="30" height="30"  data-src="{{ asset('public/images/template/Logo.svg') }}" >
                 </div>
             </a>
         </div> 
