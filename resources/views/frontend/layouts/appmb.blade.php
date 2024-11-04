b4056719364d7952f5c342c43eb4cfa8e7468d5d
cuong12121 <cuongdao12121@gmail.com>
2024-11-04 16:26:27 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -1543,8 +1543,8 @@
         <div class="zalo-mobile">
 
             <a href="https://zalo.me/0987874334" target="_blank">
-                <div style="position: fixed; bottom: 52px; right: 52px; transform: translate(0px, 0px) !important; z-index: 2147483644; border: none; visibility: visible; right: 0px; width: 30px; height: 30px;" class="zalo-chat-widget"data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height="">
-                    <img src="{{ asset('public/images/template/image.png') }}" width="30" height="30" class="lazyload" data-src="{{ asset('public/images/template/Logo.svg') }}" loading="lazy">
+                <div style="position: fixed; bottom: 52px; right: 52px; transform: translate(0px, 0px) !important; z-index: 2147483644; border: none; visibility: visible; right: 0px; width: 30px; height: 30px;" class="zalo-chat-widget lazyload" data-welcome-message="Điện Máy Người Việt rất vui khi được hỗ trợ bạn!" data-autopopup="0" data-width="" data-height="">
+                    <img src="{{ asset('public/images/template/image.png') }}" width="30" height="30"  data-src="{{ asset('public/images/template/Logo.svg') }}" loading="lazy">
                 </div>
             </a>
         </div> 
@@ -1852,7 +1852,6 @@
 
     <script>
 
-    
         $('.register-forms').click(function(){
             $("#Modal-login").modal("hide");
             $("#Modal-register").modal("show");
