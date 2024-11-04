bbdbe66bf9fd1e38c3ced252ba7df9d914920b65
cuong12121 <cuongdao12121@gmail.com>
2024-11-04 10:30:13 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -1156,7 +1156,7 @@
 
         
     </head>
-    <body class="theme-lunar-new-year d">
+    <body>
         <div class="banner-media desktop">
             <div class="" data-size="1">
                 <div class="item" data-background-color="#CF1F2F" data-order="1">
