b98bb3a037cd193b74071438527cbbd9f17c0661
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-06 22:33:01 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -2159,17 +2159,6 @@
 
             });
 
-            if ('serviceWorker' in navigator) {
-                window.addEventListener('load', () => {
-                  navigator.serviceWorker.register('{{ asset("js/servicewk.js")}}')
-                    .then((registration) => {
-                      console.log('Service Worker registered with scope:', registration.scope);
-                    })
-                    .catch((error) => {
-                      console.error('Service Worker registration failed:', error);
-                    });
-                });
-              }
             
         </script>
 
