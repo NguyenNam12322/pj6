3d2de45e21d1f58aa69d87fca74db305d2393511
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-09 21:18:07 +0700

    up

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -840,7 +840,7 @@
                             <picture>
                                   <source media="(min-width:300px)" srcset="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}">
                                   <source media="(min-width:767px)" srcset="{{ asset('https://muasamtaikho.vn/uploads/product/may-giat-invert_main_371_1020.png.webp') }}  ">
-                                  <img  src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}"   loading="eager"  sizes="(max-width: 600px) 220px, 1020px" >
+                                  <img  src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}"   loading="eager"  sizes="(max-width: 600px) auto, 1020px" >
                             </picture>
                        
                         </a>
