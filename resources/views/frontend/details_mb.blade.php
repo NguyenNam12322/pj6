041adc52d5d0eb5d68861a54814f8310a91a2cfa
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-09 16:14:08 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -842,7 +842,7 @@
                                 <picture>
                                       <source media="(min-width:300px)" srcset="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}">
                                       <source media="(min-width:767px)" srcset="{{ asset('https://muasamtaikho.vn/uploads/product/may-giat-invert_main_371_1020.png.webp') }}  ">
-                                      <img  src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}" loading="eager" >
+                                      <img  src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}" sizes="(max-width: 600px) 100vw, 1020px"  loading="eager" >
                                 </picture>
                            
                             </a>
@@ -889,7 +889,7 @@
                                <picture>
                                       <source media="(min-width:300px)" srcset="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}">
                                       <source media="(min-width:767px)" srcset="{{ asset('https://muasamtaikho.vn/uploads/product/may-giat-invert_main_371_1020.png.webp') }}  ">
-                                      <img src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}" loading="eager" >
+                                      <img src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}" sizes="(max-width: 600px) 100vw, 1020px" loading="eager" >
                                 </picture>
                             </a>
                             
