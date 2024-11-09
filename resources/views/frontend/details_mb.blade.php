08e1e98da9f0457a4cc7c1ba4d043aae1db12bb2
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-09 21:29:11 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -839,9 +839,9 @@
                     <div class="item img-main">
                         <a href="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}" data-fancybox="gallery">
                             <picture>
-                                  <source media="(max-width:767px)" srcset="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}">
-                                 
-                                  <img  src="{{ asset('https://muasamtaikho.vn/uploads/product/may-giat-invert_main_371_1020.png.webp') }}" alt="{{ @$data->Name }}" >
+                                  <source media="(min-width:300px)" srcset="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}">
+                                  <source media="(max-width:767px)" srcset="{{ asset('https://muasamtaikho.vn/uploads/product/may-giat-invert_main_371_1020.png.webp') }}  ">
+                                  <img  src="https://www.w3schools.com/tags/img_orange_flowers.jpg" alt="{{ @$data->Name }}" >
                             </picture>
                        
                         </a>
