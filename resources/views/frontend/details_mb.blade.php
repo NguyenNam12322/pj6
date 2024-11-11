71e149807287713cae4b18cd33cdc7d285a516db
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-12 00:45:54 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -40,7 +40,7 @@
      @media only screen and (max-width: 600px) {
 
 
-        .box01 .owl-carousel .owl-item, .box01 .owl-carousel .owl-item img:not(.monopoly-label) {
+        #carousel, .box01 .owl-carousel .owl-item, .box01 .owl-carousel .owl-item img:not(.monopoly-label) {
             max-width: 600px !important;
 
         }
