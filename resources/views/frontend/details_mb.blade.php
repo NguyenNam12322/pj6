e6e18b5671a860e39431262e6aeaac0dca67d03e
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-12 09:25:04 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -982,7 +982,7 @@
                     <div class="item img-main image-containers">
                         <a href="uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png" data-fancybox="gallery">
                            
-                            <img src="{{ asset('public/images/template/may-giat-invert_main_.png') }}"  width="220px" height="220px">
+                            <img src="{{ asset('public/images/template/may-giat-invert_main.webp') }}"  width="220px" height="220px">
                            
                         </a>
                         
