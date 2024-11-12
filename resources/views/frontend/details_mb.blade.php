5d4f933cd54821c9b49cd0835400958c5d622d19
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-13 05:06:46 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -1081,7 +1081,7 @@
             ?>
             <div class="item img-main image-containers">
 
-                <img src="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}" alt="{{ @$data->Name }}" loading="lazy" width="220px" height="220px">
+                <img src="{{ asset('uploads/product/1731128406_may-giat-invert_main_371_1020.png_with_bgc.png') }}" alt="{{ @$data->Name }}" width="220px" height="220px">
                 
                 @if($data->id>4720)
 
