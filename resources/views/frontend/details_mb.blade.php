b92b2d22d36736f350989ae9a15d3de6c31ac397
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-12 09:35:18 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -42,7 +42,7 @@
 
         #carousel, .box01 .owl-carousel .owl-item, .box01 .owl-carousel .owl-item img:not(.monopoly-label) {
             max-width: 335px !important;
-            height: 220px;
+            height: 150px;
 
         }
         
