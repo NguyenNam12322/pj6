d8770738c08a28ddf42db52f23a615d0adf227e6
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-12 10:37:04 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -122,7 +122,7 @@
 
         .box01{
             max-width: 600px;
-            height: 7%  !important;
+            height: 10%  !important;
         }
         .scrolling_inner{
             height: 4% !important;
