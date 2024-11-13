8ff8db0680ab9b5201de68f001ce0a01401bf2ca
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-13 22:27:30 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -20,7 +20,10 @@
 
     .scrolling, .box01 .owl-carousel .item{
         text-align: left !important;
-    }        
+    } 
+    .box01 .owl-carousel .item{
+        display: none;
+    }       
 
     .share-button .redirectCart{
         width: 49%;
