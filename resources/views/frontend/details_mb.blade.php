6fc1758ac2000bcb8842d78f8e2c809d61330c53
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-15 15:43:33 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -238,7 +238,7 @@
 
         #carousel, .box01 .owl-carousel .owl-item, .box01 .owl-carousel .owl-item img:not(.monopoly-label) {
             
-            display: none !important;
+            display: inline !important;
             
         }
         
