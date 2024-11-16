53f36e06c97dcf51e7bd7cfaa9ebf9ecbd6e16b3
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-16 10:35:04 +0700

    update

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -47,7 +47,7 @@
         
         <!-- <link rel="preload" type="text/css" href="{{ asset('css/apps.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'"> -->
         <!-- <link rel="preload" type="text/css" href="{{asset('css/dienmay.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'"> -->
-        <link rel="preload" type="text/css" href="{{asset('css/detailsfe.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'"> 
+        <!-- <link rel="preload" type="text/css" href="{{asset('css/detailsfe.css')}}" as="style" onload="this.onload=null;this.rel='stylesheet'">  -->
         <link rel="preload" type="text/css" href="{{ asset('css/detail1fe.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 
         <!-- <link rel="preload" type="text/css" href="{{ asset('css/details.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'"> -->
