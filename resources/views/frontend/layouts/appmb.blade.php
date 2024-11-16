81e3b2a2238bde56565a0526e6dbe4373573a3f7
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-11-16 09:42:15 +0700

    up

--- resources/views/frontend/layouts/appmb.blade.php
+++ resources/views/frontend/layouts/appmb.blade.php
@@ -75,9 +75,9 @@
                 display: block;
             }
 
-           /* .pay, .view-all-salient_fratured, .box_pro-benefit, .box01__tab{
+            .pay{
                 display: none;
-            }*/
+            }
 
              #nk-cart ul{
                 margin-top: 20px;
