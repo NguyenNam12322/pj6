192b6b901c966615d14f494fe7ab168174190332
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2024-12-17 04:17:25 +0700

    update

--- resources/views/frontend/details_mb.blade.php
+++ resources/views/frontend/details_mb.blade.php
@@ -1132,7 +1132,7 @@
         @endif
     </ul>
    
-    
+
 
     
    
@@ -1144,7 +1144,7 @@
             ?>
             <div class="item">
 
-                <img src="{{ asset($data->Image) }}" alt="{{ @$data->Name }}" width="220px" height="220px">
+                <img src="{{ asset($data->Image) }}?v=1.0" alt="{{ @$data->Name }}" width="220px" height="220px">
                 
                 @if($data->id>4720)
 
