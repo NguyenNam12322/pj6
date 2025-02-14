cd90e6ba6ef58f1eca3b00cc61ad208c088150c7
cuong12121 <cuongdao12121@gmail.com>
2025-02-14 14:17:29 +0700

    update

--- app/Http/Controllers/imageController.php
+++ app/Http/Controllers/imageController.php
@@ -169,7 +169,7 @@ class imageController extends AppBaseController
                 $input['link'] = $filePath;
             }    
         }
-        dd($input);
+        dd($id);
 
         $image = $this->imageRepository->update($input, $id);
 
